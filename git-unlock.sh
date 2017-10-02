2e0fe8c8539b2ad129152e8ee333cdec373a8008
Sérgio Gonçalves <sergio.goncalves@outlook.pt>
2017-10-02 22:45:33 +0100

    Cleaned up the script code

--- git-unlock.sh
+++ git-unlock.sh
@@ -1,10 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# "git unlock", a custom command for git.
 
-# Execute a "git pull" command to find errors
-PULL_RESULT=$(git pull 2>&1)
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+
+readonly lock_ref_regex='(?<=error: cannot lock ref '\'').*(?='\'': is at )'
+
+# Executes a "git pull" command to find errors
+_pull=$(git pull 2>&1)
 
 # Search for all references with locked refs and delete them
-echo "$PULL_RESULT" | grep -o -P '(?<=error: cannot lock ref '\'').*(?='\'': is at )' | while read -r ref ; do
+echo "${_pull}" | grep --only-matching --perl-regexp "${lock_ref_regex}" | while read -r ref ; do
     echo "Deleting reference '$ref'"
-	git update-ref -d $ref
+	git update-ref -d "${ref}"
 done
