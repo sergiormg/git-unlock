f14c54b790b5e1da7ae655165d4457d69e56b5ca
sergio.goncalves <sergio.goncalves@farfetch.com>
2017-10-03 18:48:48 +0100

    Replaced pull with fetch to get lock problems

--- git-unlock.sh
+++ git-unlock.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 # "git unlock", a custom command for git.
 
-set -o errexit
+#set -o errexit
 set -o pipefail
 set -o nounset
 #set -o xtrace
 
 readonly lock_ref_regex='(?<=error: cannot lock ref '\'').*(?='\'': is at )'
 
-# Executes a "git pull" command to find errors
-_pull=$(git pull 2>&1)
+# Executes a "git fetch" command to find errors
+_result=$(git fetch 2>&1)
 
 # Search for all references with locked refs and delete them
-echo "${_pull}" | grep --only-matching --perl-regexp "${lock_ref_regex}" | while read -r ref ; do
+echo "${_result}" | grep --only-matching --perl-regexp "${lock_ref_regex}" | while read -r ref ; do
     echo "Deleting reference '$ref'"
 	git update-ref -d "${ref}"
 done
