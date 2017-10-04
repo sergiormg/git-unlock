34e5feef8c6e143869aaebeb45713955031fe408
sergio.goncalves <sergio.goncalves@farfetch.com>
2017-10-04 15:06:59 +0100

    Enabled errexit. Add better user feedback.

--- git-unlock.sh
+++ git-unlock.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
 # "git unlock", a custom command for git.
 
-#set -o errexit
+set -o errexit
 set -o pipefail
 set -o nounset
-#set -o xtrace
 
 readonly lock_ref_regex='(?<=error: cannot lock ref '\'').*(?='\'': is at )'
 
 # Executes a "git fetch" command to find errors
+echo "Fetching..."
 _result=$(git fetch 2>&1)
 
-# Search for all references with locked refs and delete them
-echo "${_result}" | grep --only-matching --perl-regexp "${lock_ref_regex}" | while read -r ref ; do
-    echo "Deleting reference '$ref'"
-	git update-ref -d "${ref}"
-done
+# Search for all references with locked refs
+_errors=($(echo "${_result}" | grep -q --only-matching --perl-regexp "${lock_ref_regex}" || true))
+
+# If any, delete the locked refs
+if [ ${#_errors[@]} -gt 0 ]
+then
+	for ref in "${_errors[@]}"
+	do
+		echo "Deleting reference '$ref'"
+		#git update-ref -d "${ref}"
+	done
+else
+	echo "No problems found."
+fi
